2181944e606ceba80484c91834c1dd142ef9a4b0
Guilherme Silva <sgsouza.173@gmail.com>
2024-09-17 15:32:29 -0300

    Create class funcionario (model)

---
+++ Back-end/src/main/java/org/squad05/chatbot/models/Funcionario.java
@@ -0,0 +1,57 @@
+package org.squad05.chatbot.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "funcionarios")
+public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String setor;
+    private String cargo;
+
+    //Getters e setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSetor() {
+        return setor;
+    }
+
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
