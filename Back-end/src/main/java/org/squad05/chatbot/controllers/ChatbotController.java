f460c32fe107a27ebd3b524abf63b7e37787f588
Guilherme Silva <sgsouza.173@gmail.com>
2024-09-19 14:55:34 -0300

    Create ChatBotDTO

--- Back-end/src/main/java/org/squad05/chatbot/controllers/ChatbotController.java
+++ Back-end/src/main/java/org/squad05/chatbot/controllers/ChatbotController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.squad05.chatbot.models.ChatbotProcesso;
-import org.squad05.chatbot.models.Funcionario;
 import org.squad05.chatbot.service.ChatbotService;
 
 import java.util.List;
