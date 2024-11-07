6c4e7b36ce81f69d5f664ee8026e777ef3c58d2d
Guilherme Silva <sgsouza.173@gmail.com>
2024-11-07 13:29:05 -0300

    Adição de credenciais

--- Back-end/src/main/java/org/squad05/chatbot/configurations/CorsConfig.java
+++ Back-end/src/main/java/org/squad05/chatbot/configurations/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("") //Enderço front-end
+                .allowedOrigins("https://squad05-portodigital.github.io") //Enderço front-end
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
