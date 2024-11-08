3fc60d98d872195afc694b9afd17718a2d94b959
Guilherme Silva <sgsouza.173@gmail.com>
2024-11-08 10:11:10 -0300

    Create OpenApiConfig.java

---
+++ Back-end/src/main/java/org/squad05/chatbot/configurations/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package org.squad05.chatbot.configurations;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://back-end-chatbot-deploy.up.railway.app"));
+    }
+}
