8a57095f887d31882bacfa241b63ece3b21c8485
Guilherme Silva <sgsouza.173@gmail.com>
2024-11-24 22:56:18 -0300

    Update CorsConfig.java

--- Back-end/src/main/java/org/squad05/chatbot/configurations/CorsConfig.java
+++ Back-end/src/main/java/org/squad05/chatbot/configurations/CorsConfig.java
@@ -11,7 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://squad05-portodigital.github.io", //Endereço front-end
-                        "https://back-end-chatbot-deploy.up.railway.app") //Enderço back-end
+                        "https://back-end-chatbot-deploy.up.railway.app",
+                        "http://127.0.0.1:5500/") //Enderço do front de Ryan
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
 
